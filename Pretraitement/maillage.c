533ff045fb1d6b6ba0d40b4e26647257e3a00c43
adoradoux <adoradoux@macdoradoux.home>
2018-06-24 13:37:23 +0200

    Initialisation travail maillage

---
+++ Pretraitement/maillage.c
@@ -0,0 +1,15 @@
+//Standards headers
+#include <stdio.h>
+#include <stdlib.h>
+
+//Specific headers
+#include "../maillage.h"
+
+
+int maillage(void)
+{
+   printf("coucou\n");
+   
+   int ierror = 0;
+   return ierror;
+}
---
+++ maillage.h
@@ -0,0 +1,1 @@
+int maillage(void);
