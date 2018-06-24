8255a009b0073a1263cfd77736a6e1db88d5c3fd
adoradoux <adoradoux@macdoradoux.home>
2018-06-24 11:56:14 +0200

    Creation du fichier main

---
+++ mecaf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("***********************************************\n");
+    printf("*                                             *\n");
+    printf("* MECAF                                       *\n");
+    printf("* Resolution des équations de Navier Stokes   *\n");
+    printf("* 06/2018                                     *\n");
+    printf("*                                             *\n");
+    printf("***********************************************\n");
+    return 0;
+}
